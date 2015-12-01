2a3aa0ab0061b9a00f0a2f456d8bb1755db31da7
wuboy <wuboys@gmail.com>
2015-12-02 05:39:37 +0800

    test solidus

--- backend/lib/spree/backend.rb
+++ backend/lib/spree/backend.rb
@@ -5,8 +5,8 @@ require 'bourbon'
 require 'select2-rails'
 require 'handlebars_assets'
 
-require 'spree_core'
-require 'spree_api'
+require 'spree/core'
+require 'spree/api'
 
 require 'spree/responder'
 require 'spree/backend/action_callbacks'
